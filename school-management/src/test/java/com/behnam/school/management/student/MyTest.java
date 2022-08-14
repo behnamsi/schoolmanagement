714d3110cc6c6a71addf8036b71eabc1dc9d6164
Behnam <behnamsaqari1981@gmail.com>
2022-08-14 18:16:02 +0430

    validation update

--- school-management/src/test/java/com/behnam/school/management/student/MyTest.java
+++ school-management/src/test/java/com/behnam/school/management/student/MyTest.java
@@ -1,6 +1,6 @@
 package com.behnam.school.management.student;
 
-import com.behnam.school.management.configuration.SpringContext;
+
 import com.behnam.school.management.repository.CollegeRepository;
 import com.behnam.school.management.repository.CourseRepository;
 import com.behnam.school.management.repository.StudentRepository;
@@ -11,13 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Scanner;
+
 @SpringBootTest
 public class MyTest {
 
     @Test
     void myTst() {
-        StudentRepository repository = SpringContext.getBean(StudentRepository.class);
-        System.out.println(SpringContext.getContext());
-        System.err.println(repository.findById(3L));
+
     }
 }
